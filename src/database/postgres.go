1d04322a87d7f029d25e7939ae9e4bd1ccef8f08
Matthew Fritz <mattf@burbankparanormal.com>
2025-06-11 15:37:35 -0700

    Refactor the Postgres connection procedure into separate functions so we are not locked into one specific configuration workflow

--- src/database/postgres.go
+++ src/database/postgres.go
@@ -17,24 +17,16 @@ type PostgresDatabaseConnectionArguments struct {
 	Timezone string
 }
 
-// NewPostgresDatabaseConnection opens and initializes a Postgres connection with GORM using the supplied connection
-// arguments, a boolean describing whether to turn on "debug mode" automatically, and a slice of GORM options. Returns
-// the DB connection pointer as well as any error that may have occurred.
-func NewPostgresDatabaseConnection(
-	connectionArguments *PostgresDatabaseConnectionArguments, shouldUseDebugMode bool, gormOptions ...gorm.Option,
-) (*DatabaseConnection, error) {
-	if connectionArguments == nil {
-		return nil, fmt.Errorf("connection arguments for postgres cannot be nil")
+// MakePostgresConfigFromDSN takes a DSN string and returns a postgres.Config instance that contains it.
+func MakePostgresConfigFromDSN(DSN string) postgres.Config {
+	return postgres.Config{
+		DSN: DSN,
 	}
-	if connectionArguments.Host == "" {
-		return nil, fmt.Errorf("database host in connection arguments cannot be blank")
-	}
-	if connectionArguments.DatabaseName == "" {
-		return nil, fmt.Errorf("database name in connection arguments cannot be blank")
-	}
-	if connectionArguments.Username == "" {
-		return nil, fmt.Errorf("username in connection arguments cannot be blank")
-	}
+}
+
+// MakePostgresDSNFromConnectionArguments takes a PostgresDatabaseConnectionArguments struct pointer and returns a
+// string containing the DSN to be used during connections.
+func MakePostgresDSNFromConnectionArguments(connectionArguments *PostgresDatabaseConnectionArguments) string {
 	connectionParams := []string{
 		fmt.Sprintf("host=%s", connectionArguments.Host),
 		fmt.Sprintf("dbname=%s", connectionArguments.DatabaseName),
@@ -52,8 +44,45 @@ func NewPostgresDatabaseConnection(
 	if connectionArguments.Timezone != "" {
 		connectionParams = append(connectionParams, fmt.Sprintf("timezone=%s", connectionArguments.Timezone))
 	}
-	postgresDialector := postgres.New(postgres.Config{
-		DSN: strings.Join(connectionParams, " "),
-	})
+	return strings.Join(connectionParams, " ")
+}
+
+// MakePostgresDialectorFromConfig takes a postgres.Config instance and returns a gorm.Dialector instance that can be
+// used for opening new GORM database connections.
+func MakePostgresDialectorFromConfig(config postgres.Config) gorm.Dialector {
+	return postgres.New(config)
+}
+
+// NewPostgresDatabaseConnection opens and initializes a Postgres connection with GORM using the supplied connection
+// arguments, a boolean describing whether to turn on "debug mode" automatically, and a slice of GORM options. Returns
+// the DB connection pointer as well as any error that may have occurred.
+func NewPostgresDatabaseConnection(
+	connectionArguments *PostgresDatabaseConnectionArguments, shouldUseDebugMode bool, gormOptions ...gorm.Option,
+) (*DatabaseConnection, error) {
+	err := ValidatePostgresConnectionArguments(connectionArguments)
+	if err != nil {
+		return nil, err
+	}
+	postgresDSN := MakePostgresDSNFromConnectionArguments(connectionArguments)
+	postgresConfig := MakePostgresConfigFromDSN(postgresDSN)
+	postgresDialector := MakePostgresDialectorFromConfig(postgresConfig)
 	return NewDatabaseConnection(postgresDialector, shouldUseDebugMode, gormOptions...)
+}
+
+// ValidatePostgresConnectionArguments takes a PostgresDatabaseConnectionArguments struct pointer and returns an error
+// if any of the expected fields are missing. Returns nil if the validation checks pass.
+func ValidatePostgresConnectionArguments(connectionArguments *PostgresDatabaseConnectionArguments) error {
+	if connectionArguments == nil {
+		return fmt.Errorf("connection arguments for postgres cannot be nil")
+	}
+	if connectionArguments.Host == "" {
+		return fmt.Errorf("database host in connection arguments cannot be blank")
+	}
+	if connectionArguments.DatabaseName == "" {
+		return fmt.Errorf("database name in connection arguments cannot be blank")
+	}
+	if connectionArguments.Username == "" {
+		return fmt.Errorf("username in connection arguments cannot be blank")
+	}
+	return nil
 }
