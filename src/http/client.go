aa02dd4dbb2a3e889b0e8e6c1347dfaee450c14e
Matthew Fritz <mattf@burbankparanormal.com>
2025-12-28 13:29:56 -0800

    Refactor the http package for organization purposes

--- src/http/client.go
+++ src/http/client.go
@@ -3,27 +3,11 @@ package http
 import (
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"sync"
 )
-
-// ErrCannotCreateHTTPRequest is a sentinel error representing a failure while creating an HTTP request instance.
-var ErrCannotCreateHTTPRequest = errors.New("cannot create http request")
-
-// ErrCannotReadHTTPResponseBody is a sentinel error representing a failure while reading the body of an HTTP response.
-var ErrCannotReadHTTPResponseBody = errors.New("cannot read http response body")
-
-// ErrHTTPClientCannotBeNil is a sentinel error representing an attempt to use a nil HTTPClient pointer.
-var ErrHTTPClientCannotBeNil = errors.New("HTTPClient instance cannot be nil")
-
-// ErrHTTPRequestFailed is a sentinel error representing a failure when sending an HTTP request itself.
-var ErrHTTPRequestFailed = errors.New("http request failed")
-
-// ErrNetHTTPClientCannotBeNil is a sentinel error representing an attempt to use a nil http.Client pointer.
-var ErrNetHTTPClientCannotBeNil = errors.New("http.Client instance cannot be nil")
 
 // HTTPClient represents a struct that provides basic HTTP client connectivity capabilities.
 type HTTPClient struct {
--- src/http/http.go
+++ src/http/http.go
@@ -2,64 +2,6 @@ package http
 
 import (
 	"context"
-	"net/http"
-)
-
-// HTTPMethod is a type used to represent the HTTP method that should be used when sending a request.
-type HTTPMethod int
-
-// String returns the string representation of the HTTP method.
-func (hm HTTPMethod) String() string {
-	switch hm {
-	case HTTPMethodConnect:
-		return http.MethodConnect
-	case HTTPMethodDelete:
-		return http.MethodDelete
-	case HTTPMethodGet:
-		return http.MethodGet
-	case HTTPMethodHead:
-		return http.MethodHead
-	case HTTPMethodOptions:
-		return http.MethodOptions
-	case HTTPMethodPatch:
-		return http.MethodPatch
-	case HTTPMethodPost:
-		return http.MethodPost
-	case HTTPMethodPut:
-		return http.MethodPut
-	case HTTPMethodTrace:
-		return http.MethodTrace
-	}
-	return ""
-}
-
-const (
-	// HTTPMethodConnect represents a CONNECT request over HTTP.
-	HTTPMethodConnect HTTPMethod = iota
-
-	// HTTPMethodDelete represents a DELETE request over HTTP.
-	HTTPMethodDelete
-
-	// HTTPMethodGet represents a GET request over HTTP.
-	HTTPMethodGet
-
-	// HTTPMethodHead represents a HEAD request over HTTP.
-	HTTPMethodHead
-
-	// HTTPMethodOptions represents an OPTIONS request over HTTP.
-	HTTPMethodOptions
-
-	// HTTPMethodPatch represents a PATCH request over HTTP.
-	HTTPMethodPatch
-
-	// HTTPMethodPost represents a POST request over HTTP.
-	HTTPMethodPost
-
-	// HTTPMethodPut represents a PUT request over HTTP.
-	HTTPMethodPut
-
-	// HTTPMethodTrace represents a TRACE request over HTTP.
-	HTTPMethodTrace
 )
 
 // HTTPAwareClient is an interface that represents a client used for performing HTTP operations.
